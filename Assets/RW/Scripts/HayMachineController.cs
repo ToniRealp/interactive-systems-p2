e37afc96a6a05ebd5bd566ff5a9753f8d5055888
ToniRealp <tonirealp@gmail.com>
2022-05-04 21:38:18 +0200

    Adeed base project

---
+++ Assets/RW/Scripts/HayMachineController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachineController : MonoBehaviour
+{
+    public int movementSpeed = 10;
+    public int horizontalBoundary = 22;
+    public GameObject hayBalePrefab;
+    public Transform haySpawnPoint;
+    public float shootInterval;
+    private float shootTimer;
+
+    void Update()
+    {
+        UpdateMovement();
+        UpdateShooting();
+    }
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary)
+        {
+            
+            transform.Translate(transform.right * (-movementSpeed * Time.deltaTime));
+        }
+        else if(horizontalInput > 0 && transform.position.x < horizontalBoundary)
+        {
+            transform.Translate(transform.right * (movementSpeed * Time.deltaTime));
+        }
+    }
+    
+    private void UpdateShooting() {
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space)) {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnPoint.position, Quaternion.identity);
+    }
+}
